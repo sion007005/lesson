e93048b2a4613cd5c0d23f4e58dd2128a53c9b57
문한주(163229) <luvet@shinsegae.com>
2020-02-13 20:31:27 +0900

    [2주차] 코드리뷰 (검색창 UX개선, 정렬 코드정리, 버그픽스)

--- clipboard.js
+++ clipboard.js
@@ -186,21 +186,23 @@
       return listList;
     };
     const listList = divide(timelineList, ITEM_PER_ROW);
+    /* FIXME 검색 후 정렬버튼 누를 시, 검색결과 안에서만 정렬할 수 있도록 해주세요
+    이후 검색창 비웠을 때는 검색 중에 정렬한 순서 유지 해주세요 */
 
+    // FIXME 검색창 비웠을 때 모든사진 나오도록 확장해주세요 (수정완료)
     const filter = e => {
-      //BUG질문 : myboard파일에서는 마지막에 펭수사진 옆에 그냥 빈칸인데
-      //왜 여기서는 빈사진 나오면 에러가 날까요..?
       $el.lastElementChild.firstElementChild.innerHTML = '';
-      let searchText = e.target.value;
-      const resultList =
-        timelineList.filter(x => x.text.includes(`${searchText}`)) ||
-        timelineList.filter(x => x.name.includes(`${searchText}`));
+      const searchText = e.target.value;
+      /* BUG text만 검색되고 name은 검색되지 않고 있습니다 (수정완료) */
+      const resultList = timelineList.filter(x => x.text.includes(searchText) || x.name.includes(searchText));
 
       divide(resultList, ITEM_PER_ROW).forEach(list => {
         let html = '';
+        // COMMENT 왜 reduce 걷어내고 인덱스로 for문을 돌리신건가요?
         for (let i = 0; i < list.length; i++) {
           const img =
-            (`${list[i].img}` || '') &&
+            // BUG 변수값을 접근하는데 템플릿리터럴로 감싸서 부른 특별한 이유가 있을까요? (수정완료)
+            (list[i].img || '') &&
             `
             <a href="javascript:;">
               <div class="eLAPa">
@@ -224,66 +226,30 @@
       `
         );
       });
-      e.target.value = '';
     };
 
     const sort = e => {
       $el.lastElementChild.firstElementChild.innerHTML = '';
+      /* FIXME 불필요한 장대한 분기문입니다, 읽기도 힘들고 수정하기도 힘듭니다
+      분기문은 예외적인 일부로직을 처리하는 데만 제한적으로 구조를 잡아주시고
+      메인로직 자체를 분기문으로 감싸는 로직은 지양해주세요
+      여기서는 sort 안에 들어가는 콜백 정도만 분기해도 충분할 것 같습니다 (수정완료) */
+      let comparator;
       if (e.target.id === 'newButton') {
-        timelineList.sort((x, y) => {
-          return (
-            y.timestamp.replace(/\s|\/|:/gi, '') -
-            x.timestamp.replace(/\s|\/|:/gi, '')
-          );
-        });
-        const listList = divide(timelineList, ITEM_PER_ROW);
-        listList.forEach(list => {
-          const gridItem = (($parent, list) => {
-            let $el;
-
-            const create = () => {
-              render(list);
-              $el = $parent.lastElementChild;
-            };
-
-            const render = list => {
-              const html = list.reduce((html, data) => {
-                const img =
-                  (data.img || '') &&
-                  `
-                <a href="javascript:;">
-                <div class="eLAPa">
-                    <div class="KL4Bh"><img class="FFVAD" decoding="auto" src="${common.IMG_PATH}${data.img}" style="object-fit: cover;"></div>
-                </div>
-            </a>
-                `;
-                html += `
-              <div class="v1Nh3 kIKUG _bz0w">
-                ${img}
-              </div>
-              `;
-                return html;
-              }, '');
-
-              $parent.insertAdjacentHTML(
-                'beforeend',
-                `
-                      <div class="Nnq7C weEfm">
-                      ${html}
-                      </div>
-                  `
-              );
-            };
-            create();
-            return {$el};
-          })(grid.$el.lastElementChild.firstElementChild, list);
-        });
+        comparator = (x, y) => {
+            const a = x.timestamp.replace(/\s|\/|:/gi, '');
+            const b = y.timestamp.replace(/\s|\/|:/gi, '');
+            return b - a;
+          }
       } else if (e.target.id === 'hotButton') {
-        timelineList.sort((x, y) => {
-          const a = x.clipCount * 1 + x.commentCount * 2;
-          const b = y.clipCount * 1 + y.commentCount * 2;
-          return b - a;
-        });
+        comparator = (x, y) => {
+            const a = x.clipCount * 1 + x.commentCount * 2;
+            const b = y.clipCount * 1 + y.commentCount * 2;
+            return b - a;
+          }
+      }
+        
+        timelineList.sort(comparator);
         const listList = divide(timelineList, ITEM_PER_ROW);
         listList.forEach(list => {
           const gridItem = (($parent, list) => {
@@ -326,7 +292,6 @@
             return {$el};
           })(grid.$el.lastElementChild.firstElementChild, list);
         });
-      }
     };
 
     const render = () => {
@@ -369,9 +334,14 @@
 
     create();
 
+    /* FIXME 컴포넌트 안에서 컴포넌트 밖까지 DOM 룩업하지 마세요
+    동일ID를 가진 엘리먼트가 컴포넌트 밖에 없다는 보장이 없습니다
+    영향범위를 예측할 수 없는 견고하지 못한 코드입니다
+    컴포넌트 안에서는 뭐든 컴포넌트 안에서만 해주세요 ($el 내부만 룩업) */
     const searchBar = document.getElementById('searchbar');
-    searchBar.addEventListener('change', filter);
+    searchBar.addEventListener('keyup', filter);
 
+    // TODO 같은 리스너를 쓸거면 두 버튼에 각각 등록하는 것이 아니라, 부모 엘리먼트에 리스너를 등록 해주세요
     const newestOrderButton = document.getElementById('newButton');
     newestOrderButton.addEventListener('click', sort);
 
